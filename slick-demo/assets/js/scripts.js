84b26f363836425ed1a1b08e1fe6fd722c4506d6
Travis Haskin <thaskin@ualr.edu>
2025-12-02 16:56:54 +0000

    slideshow

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -1,10 +1,16 @@
+
 $(document).ready(function() {
 
-$('.autoplay').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
 
-});
+
 });
